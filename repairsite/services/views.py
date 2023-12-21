0cd2812f9cc22005e74a0541b935b0ba1c82f9a5
Alexey <idolexey@yandex.ru>
2023-12-21 18:28:32 +0500

    Min site

--- repairsite/services/views.py
+++ repairsite/services/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+menu = [
+    {'url_title': 'Главная', 'url_name': 'home'},
+    {'url_title': 'О сайте', 'url_name': 'about'},
+    {'url_title': 'Каталог', 'url_name': 'catalog'},
+]
+
+def home(request):
+    data = {
+        'title': 'Главная',
+        'menu': menu,
+    }
+    return render(request, 'services/home.html', context=data)
+
+def about(request):
+    data = {
+        'title': 'О сайте',
+        'menu': menu,
+    }
+    return render(request, 'services/about.html', context=data)
+
+def catalog(request):
+    data = {
+        'title': 'Каталог услуг',
+        'menu': menu,
+    }
+    return render(request, 'services/catalog.html', context=data)
